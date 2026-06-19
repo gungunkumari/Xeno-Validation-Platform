b236ca57cbcb0b8a00ad690b7ad7141aefe99863
Saanv <saanv@github.com>
2026-06-19 11:20:58 +0530

    Connect backend to Supabase

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,4 @@
 from pydantic_settings import BaseSettings
-from sqlalchemy.engine import URL
 
 
 class Settings(BaseSettings):
@@ -7,24 +6,9 @@ class Settings(BaseSettings):
     APP_VERSION: str = "1.0.0"
     DEBUG: bool = False
 
-    DB_USER: str = "postgres"
-    DB_PASSWORD: str
-    DB_HOST: str = "localhost"
-    DB_PORT: int = 5432
-    DB_NAME: str = "xeno_assignment"
+    DATABASE_URL: str
 
     ALLOWED_ORIGINS: list[str] = ["http://localhost:3000"]
-
-    @property
-    def DATABASE_URL(self):
-        return URL.create(
-            drivername="postgresql+psycopg2",
-            username=self.DB_USER,
-            password=self.DB_PASSWORD,
-            host=self.DB_HOST,
-            port=self.DB_PORT,
-            database=self.DB_NAME,
-        )
 
     class Config:
         env_file = ".env"
